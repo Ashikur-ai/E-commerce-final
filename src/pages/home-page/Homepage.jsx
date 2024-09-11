7139e2996c03cdfd6e520c4ef8862be9f967c728
rezaulislam54 <rezaulislam5490@gmail.com>
2024-09-11 22:11:17 +0600

    navber ,banner and sponser section complete

---
+++ src/pages/home-page/Homepage.jsx
@@ -0,0 +1,13 @@
+import Sponser from "../../components/Sponser";
+import Banner from "./Banner";
+
+const Homepage = () => {
+  return (
+    <div>
+      <Banner></Banner>
+      <Sponser></Sponser>
+    </div>
+  );
+};
+
+export default Homepage;
